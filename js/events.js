5d2736e56536da043f51a6d4bb3ab970dd1ed10f
rootnure <nur.diu.2791@gmail.com>
2023-08-14 17:24:01 +0600

    Multiple ways to add onclick event handler

--- js/events.js
+++ js/events.js
@@ -1,1 +1,20 @@
-console.log('This is separate JS file');
+// Option 1: add directly on onclick attribute value
+
+// Option 2: add onclick function
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+// Option 3: add onclick function by selecting an element
+const makeBlueBtn = document.getElementById('make-blue');
+makeBlueBtn.onclick = makeBlue;
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+// Option 3 (another)
+const aquaBtn = document.getElementById('make-aqua');
+aquaBtn.onclick = function makeAqua() {
+    document.body.style.backgroundColor = 'aqua';
+}
