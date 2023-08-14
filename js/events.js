d665c7eaf619cd629a68be2a45b877b36fbf8c34
rootnure <nur.diu.2791@gmail.com>
2023-08-14 17:46:54 +0600

    onclick event handler html example added

--- js/events.js
+++ js/events.js
@@ -1,11 +1,16 @@
 // Option 1: add directly on onclick attribute value
+// <button onclick="console.log(100)">Another Button</button>
 
-// Option 2: add onclick function
+// Option 2: add onclick function on the html element
+// <button onclick="makeRed()">Make Red</button>
+
 function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
 
 // Option 3: add onclick function by selecting an element
+// <button id="make-blue">Make Blue</button>
+
 const makeBlueBtn = document.getElementById('make-blue');
 makeBlueBtn.onclick = makeBlue;
 
